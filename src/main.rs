58aff30b46f8e34ec242a681bf7a36d5d25a743f
Todd Kennedy <95242064+tkennedy1-godaddy@users.noreply.github.com>
2021-12-10 09:59:13 -0800

    add link formatter

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 use chrono::{DateTime, Local};
+use std::env;
 
 const DATE_FORMAT: &str = "%A, %b %e, %Y";
+const DATE_FORMAT_DISPLAY: &str = "%B %d";
+const DATE_FORMAT_LINK: &str = "%Y-%m-%d";
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
     let now: DateTime<Local> = Local::now();
-    print!(
-        "# {}\n\n## TODO\n* [ ]\n\n## In Progress\n*\n\n## Done\n*\n\n## Notes\n\n[back to index](index.md)",
-        now.format(DATE_FORMAT).to_string()
-    );
+
+    let flag = args.get(1);
+    match flag {
+        Some(e) => 
+            match e.as_str() {
+                "--link" => print!(
+                        "[{}]({}.md)",
+                        now.format(DATE_FORMAT_DISPLAY),
+                        now.format(DATE_FORMAT_LINK)
+                    ),
+                _ => print!("NOT SUPPORTED")
+            }
+        None => print!(
+                "# {}\n\n## TODO\n* [ ]\n\n## In Progress\n*\n\n## Done\n*\n\n## Notes\n\n[back to index](index.md)",
+                now.format(DATE_FORMAT).to_string()
+            ),
+
+    };
 }
